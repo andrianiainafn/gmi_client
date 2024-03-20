5986720050f7e89ae7502fb6f2de691f3a424d71
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-20 12:13:32 +0300

    feat: make request and material stat dynamic

--- src/app/dashboard/_components/number_indicator/progress_circular.tsx
+++ src/app/dashboard/_components/number_indicator/progress_circular.tsx
@@ -1,60 +1,19 @@
 "use client"
-import { Card, ProgressCircle } from '@tremor/react';
+import MaterialStatisticItem from "@/app/dashboard/_components/number_indicator/material_statistic_item";
+import {useFetchMaterialStat} from "@/app/dashboard/_hooks/dashboard_hook";
+import {IMaterialStatItem} from "@/app/dashboard/_services/definition";
 
-export function ProgressCircleUsageExample() {
+export function MaterialStatisticContainer() {
+    const {data,isSuccess,isLoading}=useFetchMaterialStat()
     return (
         <div className="flex space-x-3">
-            <div className="space-y-3">
-                <Card className="mx-auto max-w-sm">
-                    <div className="flex justify-start space-x-5 items-center">
-                        <ProgressCircle value={24} color="teal-500" size="md">
-                            <span className="text-xs font-medium text-slate-700 dark:text-white">24%</span>
-                        </ProgressCircle>
-                        <div>
-                            <p className="text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong font-medium">
-                                12/50 (24%)
-                            </p>
-                            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-                                Number of equipment under maintenance
-                            </p>
-                        </div>
-                    </div>
-                </Card>
-            </div>
-            <div className="space-y-3">
-                <Card className="mx-auto max-w-sm">
-                    <div className="flex justify-start space-x-5 items-center">
-                        <ProgressCircle value={50} color="teal-500" size="md">
-                            <span className="text-xs font-medium text-slate-700 dark:text-white">50%</span>
-                        </ProgressCircle>
-                        <div>
-                            <p className="text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong font-medium">
-                                25/50 (50%)
-                            </p>
-                            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-                                Number of equipment in service or activity
-                            </p>
-                        </div>
-                    </div>
-                </Card>
-            </div>
-            <div className="space-y-3">
-                <Card className="mx-auto max-w-sm">
-                    <div className="flex justify-start space-x-5 items-center">
-                        <ProgressCircle value={26} color="teal-500" size="md">
-                            <span className="text-xs font-medium text-slate-700 dark:text-white">26%</span>
-                        </ProgressCircle>
-                        <div>
-                            <p className="text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong font-medium">
-                                13/50 (26%)
-                            </p>
-                            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-                                Number of equipment out of service
-                            </p>
-                        </div>
-                    </div>
-                </Card>
-            </div>
+            {
+                isSuccess && (
+                    data.data.materialStats.map((elem:IMaterialStatItem,key:number)=>(
+                        <MaterialStatisticItem key={key} total={data.data.materialNumber} label={elem.statName} number={elem.statNumber}/>
+                    ))
+                )
+            }
         </div>
     );
 }
