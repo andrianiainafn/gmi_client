bbe5294d94ff7bcfddd78e2657c8792fac3c51d0
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-22 23:30:38 +0300

    feat : add tabs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,7 +87,6 @@ const config = {
         dark: '#232A3C',
         medium: '#293245',
         border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
@@ -176,7 +175,6 @@ const config = {
   ],
   plugins: [
     require('@headlessui/tailwindcss'),
-    require('@tailwindcss/forms'),
     require("tailwindcss-animate"),
     require('@headlessui/tailwindcss'),
     addVariablesForColors,
