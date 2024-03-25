1b3db4114582dc7b456249ee912e5a15dd83d6a4
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-25 09:39:12 +0300

    feat: finished create material form

--- src/app/dashboard/material/_components/form/create_material_form.tsx
+++ src/app/dashboard/material/_components/form/create_material_form.tsx
@@ -1,6 +1,9 @@
+"use client"
 import React from 'react';
-import {useForm} from "react-hook-form";
+import {useForm,SubmitHandler} from "react-hook-form";
 import {Button} from "@/components/ui/button";
+import {useFetchAllStatus} from "@/app/dashboard/material/_hooks/material_hook";
+import {IMaterialStatus} from "@/app/dashboard/material/_services/definition";
 
 interface IFormInput{
     name:string,
@@ -10,27 +13,46 @@ interface IFormInput{
 }
 
 const CreateMaterialForm = () => {
+    const {data,isLoading,isSuccess} = useFetchAllStatus()
     const {
         register,
         handleSubmit,
         formState:{errors,isSubmitting}
     } = useForm<IFormInput>()
+    const onSubmit: SubmitHandler<IFormInput> = (data)=>{
+        console.log(data)
+    }
     return (
-        <form>
-            <div className="grid w-full  items-center gap-1.5">
-                <label htmlFor='design'>Name</label>
-                <input type='text'  placeholder='Ex: Cable Rj45'
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid w-full  items-center gap-1.5 space-y-2">
+                <label htmlFor='name'>Name</label>
+                <input type='text'  placeholder='Ex: Cable Rj45' {...register("name")}
                        className='outline-none p-2 border '  />
-                <label htmlFor='design'>Serial number</label>
-                <input type='text'  placeholder='Ex: Cable Rj45'
+                <label htmlFor='serialNumber'>Serial number</label>
+                <input type='text'   placeholder='Ex: E3412' {...register("serialNumber")}
                        className='outline-none p-2 border '  />
-                <label htmlFor='design'>Description</label>
-                <textarea   placeholder='Ex: Cable Rj45'
+                <label htmlFor='statusId'>Status</label>
+                <select {...register("statusId")} className="bg-transparent border px-2 py-3">
+                    <option disabled selected value="">Choose an option</option>
+                    {
+                        isSuccess && (
+                            <>
+                                {
+                                    data.data.map((elem:IMaterialStatus,key:number)=>(
+                                        <option value={elem.materialStatusId}>{elem.materialStatusName}</option>
+                                    ))
+                                }
+                            </>
+                        )
+                    }
+                </select>
+                <label htmlFor='description'>Description</label>
+                <textarea   placeholder='Ex: Cable 2m red' {...register("description")}
                        className='outline-none p-2 border '  />
-                <Button className="bg-teal-500" >
+                <Button className="bg-teal-500" type="submit" >
                     Create material
                 </Button>
-                <Button variant="secondary">
+                <Button variant="secondary" type="reset" >
                     Cancel
                 </Button>
             </div>
