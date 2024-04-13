423cc18422074df6f7eb4c3f799a28de38143bc5
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-04-13 22:00:46 +0300

    refactor: remove material item to another component

--- src/app/dashboard/material/_components/table/table_row.tsx
+++ src/app/dashboard/material/_components/table/table_row.tsx
@@ -2,99 +2,15 @@
 import React from 'react';
 import {useMaterialStore} from "@/app/dashboard/material/_state/material_state";
 import {IMaterial} from "@/app/dashboard/material/_services/definition";
-import {Button} from "@/components/ui/button";
-import {CircleX, Pencil} from "lucide-react";
-import { formatDistanceToNow } from 'date-fns';
+import MaterialItem from "@/app/dashboard/material/_components/table/material_item";
 
 const TableRow = () => {
     const material = useMaterialStore.use.material()
-    const formatDate = (apiDate:Date) => {
-        const distance = formatDistanceToNow(new Date(apiDate), { addSuffix: true, includeSeconds: true });
-        const distanceWithoutAbout = distance.replace('about', '');
-        return distanceWithoutAbout
-    };
     return (
-        <tbody className="text-center font-light text-sm text-gray-600 ">
+        <tbody className=" ">
             {
                 material.map((elem:IMaterial,key:number)=>(
-                    <tr key={key} className={`${(key + 1 ) % 2 === 0 ? "bg-gray-100 dark:bg-gray-950" : ""} py-4`}>
-                        <td>
-                            {elem.materialName}
-                        </td>
-                        <td >
-                            {elem.serialNumber}
-                        </td>
-                        {
-                            elem.actualStatus === "Under Maintenance" && (
-                                <td >
-                                    <span className="  bg-blue-400 bg-opacity-30 text-blue-500 py-1 px-3 rounded-lg ">
-                                        {elem.actualStatus}
-                                    </span>
-                                </td>
-                            )
-                        }
-                        {
-                            elem.actualStatus === "In service" && (
-                                <td >
-                                    <span className="bg-teal-500 bg-opacity-30 text-teal-700 py-1 px-3 rounded-lg">
-                                        {elem.actualStatus}
-                                    </span>
-                                </td>
-                            )
-                        }
-                        {
-                            elem.actualStatus === "Out of service" && (
-                                <td >
-                                    <span className="bg-red-500 bg-opacity-30 text-red-600 py-1 px-3 rounded-lg">
-                                        {elem.actualStatus}
-                                    </span>
-                                </td>
-                            )
-                        }
-                        <td>
-                            {elem.description}
-                        </td>
-                        <td>
-                            {
-                                elem.account ? (
-                                    <>
-                                        {elem.account}
-                                    </>
-                                ):(
-                                    <>
-                                        no account associated
-                                    </>
-                                )
-                            }
-                        </td>
-                        <td className="text-black">
-                            {
-                                elem.createdAt !== null ? (
-                                    <>
-                                        {formatDate(elem.createdAt!)}
-                                    </>
-                                ):(
-                                    <>
-
-                                    </>
-                                )
-                            }
-                        </td>
-                        <td className="flex space-x-2 items-center">
-                            <Button variant="secondary" className=" flex w-[7vw] items-center space-x-2" >
-                                <span>
-                                    Edit
-                                </span>
-                                <Pencil size={16} />
-                            </Button>
-                            <Button variant="destructive" className="flex items-center space-x-2 w-[7vw]">
-                                <span>
-                                    Delete
-                                </span>
-                                <CircleX size={16} />
-                            </Button>
-                        </td>
-                    </tr>
+                    <MaterialItem key={key} rank={key} elem={elem}/>
                 ))
             }
         </tbody>
