419be710b7b95062c05b62fb13ea9f8e2c04e2c4
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-04-14 21:29:45 +0300

    feat : add functionality to find user by name or email

--- src/app/dashboard/user/_services/user_service.ts
+++ src/app/dashboard/user/_services/user_service.ts
@@ -2,8 +2,9 @@ import {httpClient} from "@/lib/axios";
 
 import {EXAMPLE_ENDPOINTS} from "@/app/feat-exemple/_services/endpoint";
 import {IExampleDto} from "@/app/dashboard/user/_services/definition";
+import {USER_ENDPOINTS} from "@/app/dashboard/user/_services/endpoint";
 
-class ExampleService{
+class UserService{
     public createExample(exampleToCreate: IExampleDto){
         return httpClient.post(EXAMPLE_ENDPOINTS.CREATE,exampleToCreate)
     }
@@ -11,14 +12,18 @@ class ExampleService{
         return httpClient.get(EXAMPLE_ENDPOINTS.GET.replace("id",exampleId))
     }
 
-    public getAllExample(){
-        return httpClient.get(EXAMPLE_ENDPOINTS.GET_ALL)
+    public getAllUser(page:number,size:number){
+        return httpClient.get(USER_ENDPOINTS.GET_ALL)
     }
     public updateExample(exampleToUpdate: IExampleDto,id:string){
         return httpClient.put(EXAMPLE_ENDPOINTS.UPDATE.replace("id",id),exampleToUpdate)
+    }
+    public searchUser(emailOrName:string){
+        console.log(USER_ENDPOINTS.SEARCH.replace("email-or-name",emailOrName))
+        return httpClient.get(USER_ENDPOINTS.SEARCH.replace("email-or-name",emailOrName))
     }
     public deleteExampleByExampleId(exampleId: string){
         return httpClient.get(EXAMPLE_ENDPOINTS.DELETE.replace("id",exampleId))
     }
 }
-export const exampleService = new ExampleService()
+export const userService = new UserService()
