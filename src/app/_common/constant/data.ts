0380acb5cb3882728be5096cac27babde73ed2c3
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-04-14 21:32:00 +0300

    feat : add animated tooltip to illustrate owner

--- src/app/_common/constant/data.ts
+++ src/app/_common/constant/data.ts
@@ -3,3 +3,144 @@ export const requestStatus:string[]=[
     "PENDING",
     "REJECTED"
 ]
+export const people = [
+    {
+
+        accountId: "1",
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@example.com",
+        profileUrl: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+        roles: [
+            {
+                roleId: "1",
+                roleName: "Software Engineer",
+                account: [],
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }
+        ],
+        department: {
+            departmentId: "1",
+            departmentName: "Engineering",
+            accounts: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+    },
+    {
+        accountId: "2",
+        firstname: "Robert",
+        lastname: "Johnson",
+        email: "robert.johnson@example.com",
+        profileUrl: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        roles: [
+            {
+                roleId: "2",
+                roleName: "Product Manager",
+                account: [],
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }
+        ],
+        department: {
+            departmentId: "2",
+            departmentName: "Product Management",
+            accounts: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+    },
+    {
+        accountId: "3",
+        firstname: "Jane",
+        lastname: "Smith",
+        email: "jane.smith@example.com",
+        profileUrl: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        roles: [
+            {
+                roleId: "3",
+                roleName: "Data Scientist",
+                account: [],
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }
+        ],
+        department: {
+            departmentId: "3",
+            departmentName: "Data Science",
+            accounts: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+    },
+    {
+        accountId: "4",
+        firstname: "Emily",
+        lastname: "Davis",
+        email: "emily.davis@example.com",
+        profileUrl: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        roles: [
+            {
+                roleId: "4",
+                roleName: "UX Designer",
+                account: [],
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }
+        ],
+        department: {
+            departmentId: "4",
+            departmentName: "Design",
+            accounts: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+    },
+    {
+        accountId: "5",
+        firstname: "Tyler",
+        lastname: "Durden",
+        email: "tyler.durden@example.com",
+        profileUrl: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+        roles: [
+            {
+                roleId: "5",
+                roleName: "Soap Developer",
+                account: [],
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }
+        ],
+        department: {
+            departmentId: "5",
+            departmentName: "Development",
+            accounts: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+    },
+    {
+        accountId: "6",
+        firstname: "Dora",
+        lastname: "The Explorer",
+        email: "dora.explorer@example.com",
+        profileUrl: "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+        roles: [
+            {
+                roleId: "6",
+                roleName: "Explorer",
+                account: [],
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }
+        ],
+        department: {
+            departmentId: "6",
+            departmentName: "Adventure",
+            accounts: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+    }
+];
