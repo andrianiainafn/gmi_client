cbe8eb9d9fb519c1299bb1e9b99f00e9898c5ea2
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-04-16 11:22:04 +0300

    feat : add search bar for user

--- src/app/dashboard/request/_components/tab/request_bar.tsx
+++ src/app/dashboard/request/_components/tab/request_bar.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Search} from "lucide-react";
 
 const RequestBar = () => {
     return (
-        <div>
-            
+        <div className="flex border justify-between border-gray-200 dark:border-gray-500 items-center py-2 px-2 w-[300px] ">
+            <input placeholder="Search..." className="outline-none bg-none bg-transparent" />
+            <Search className="flex flex-en text-gray-200"/>
         </div>
     );
 };
