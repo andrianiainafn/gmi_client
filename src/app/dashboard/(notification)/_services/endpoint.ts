0e6281fbf677f2988d1ba6aac29518aabed6d26d
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-04-28 09:41:19 +0300

    feat: add functionality mark notification as read

--- src/app/dashboard/(notification)/_services/endpoint.ts
+++ src/app/dashboard/(notification)/_services/endpoint.ts
@@ -2,6 +2,7 @@ export enum NOTIFICATION_ENDPOINTS{
     CREATE = "/notification",
     GET_REQUEST="/notification/request",
     GET="/example/:id",
+    MARK_AS_READ="/notification/id",
     UPDATE="/example/:id",
     DELETE="/example/:id",
     GET_ALL_TECH= "/technology?q=query"
