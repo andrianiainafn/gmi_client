6d98c83184dc27a2509970d50a0b105983820476
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-04-28 09:36:22 +0300

    fix: overflow error

--- src/app/dashboard/(notification)/_components/notification_container.tsx
+++ src/app/dashboard/(notification)/_components/notification_container.tsx
@@ -18,7 +18,7 @@ const NotificationContainer = (props:Props) => {
     <>
         {
             isShow && (
-                <div className="bg-white shadow-sm border-[1px] border-gray-200 dark:border-none dark:bg-gray-950 z-20 p-2 rounded-lg h-[80vh] flex flex-col space-y-3 bottom-5  w-[25vw] absolute top-[10vh] right-0">
+                <div className="bg-white shadow-sm border-[1px] border-gray-200 dark:border-none dark:bg-gray-950 z-20 p-2 rounded-lg h-[80vh] overflow-y-scroll flex flex-col space-y-3 bottom-5  w-[25vw] absolute top-[10vh] right-0">
                     <NotificationBar HandleClickClose={setIsShow} isShow={isShow} request={[]}/>
                 </div>
             )
