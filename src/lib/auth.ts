9b79939b3a58a2c215c5c1e20330a8aa5b0369b9
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-22 23:17:47 +0300

    fix 500 error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -59,7 +59,7 @@ export const handlers = NextAuth({
     },
     callbacks:{
         async signIn({ user, account, profile, email, credentials}){
-            const response = await axios.post("https://gmiapi-production.up.railway.app/api",{
+            const response = await axios.post("https://gmiapi-production.up.railway.app/api/auth/user",{
                 firstname:user.name,
                 email:user.email,
                 lastname:user.name,
